508b04de2898173417d5079c004bb5e7034bc221
Ian F <ianmichael.fennie@haw-hamburg.de>
2017-10-26 12:52:25 +0200

    Optimize further

--- SS1_Lab1.m
+++ SS1_Lab1.m
@@ -2,23 +2,20 @@ clear
 close all
 %% Problem 1
 v1A = [1 2 3 4 5]
-v1B = [3 -1 2]'
+v1B = [3 -1 2].'
 v1C = 0:0.1:10
 v1D = 10:-0.1:0
 
 %% Problem 2
 v2A = [4 2 5];
 v2B = [3 8 9];
-magV2A = norm(v2A);
-magV2B = norm(v2B);
 dotV2 = dot(v2A, v2B);
-magV2C = dotV2/(magV2A*magV2B)
-thetaV2C = acos(magV2C)
+magV2C = dotV2/(norm(v2A)*norm(v2B))
+thetaV2C = acosd(magV2C)
 
 %% Problem 3
-k = 0:100;
-sum3A = sum(k)
-sum3B1 = sum(k.^2)
+sum3A = sum(0:100)
+sum3B1 = sum([0:100].^2)
 
 %% Problem 4
 arg = -pi/2;
@@ -38,15 +35,29 @@ sum5 = mysum(2,3)
 
 %% Problem 6
 f = [2 5];
-t = linspace(0,2,360);
+t1 = linspace(0,1,360);
+t2 = linspace(1,2,360);
+
+y1 = cos(2*pi*f(1)*t1);
+y2 = cos(2*pi*f(1)*t2);
+
+t = [t1, t2];
+y = [y1, y2];
+
 figure 'Name' 'Problem 6a (2Hz)'
-y1 = cos(2*pi*f(1)*t);
-plot(t, y1);
+plot(t, y);
+
 figure 'Name' 'Problem 6b'
-plot(t, arrayfun(@prob6, t));
+y3 = cos(2*pi*f(1)*t1);
+y4 = cos(2*pi*f(2)*t2);
+
+t = [t1, t2];
+y = [y3, y4];
+
+plot(t, y);
 
 %% Problem 7
-figure
+figure 'Name' 'Problem 7'
 s = tf('s');
 RC = 10;
 H = 1/(RC*s+1);
@@ -55,14 +66,4 @@ bode(H, {0, 1000});
 %% Function Definitions
 function z = mysum(x,y)
 z = x + y;
-end
-
-function y = prob6(x)
-f1 = 2;
-f2 = 5;
-if lt(x,1)
-    y = cos(2*pi*f1*x);
-else
-    y = cos(2*pi*f2*x);
-end
 end
